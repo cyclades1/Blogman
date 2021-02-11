9dcb5a53b82c4b36fa27f7a406aa088a9796b6e7
Abhishek Kumar Dubey <abhishekdubey007111@gmail.com>
2021-02-11 13:13:54 +0530

    added api for blogs and improved the user api

--- Social/urls.py
+++ Social/urls.py
@@ -15,34 +15,80 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers, serializers, viewsets
 from django.contrib.auth.models import User
+from blogs.models import Blog
+
+#DRF imports
+from rest_framework import routers, serializers, viewsets, status
+from rest_framework.decorators import api_view, action
+from rest_framework.response import Response
 
 admin.site.site_header = "Social Admin"
 admin.site.site_title = "Social Admin Portal"
 admin.site.index_title = "Welcome to Social Portal"
 
 # serializer for API representation
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-	class Meta:
-		model = User
-		fields = ['username', 'email']
+class UserSerializer(serializers.ModelSerializer):
 
+	class Meta:
+		model     = User
+		fields    = ['username', 'email']
+
+
+class BlogSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Blog
+        fields = ['title','author', 'content', 'date', 'genre']
+
+    
 
 # Viewset to define the behavior of view
+
 class UserViewSet(viewsets.ModelViewSet):
-	queryset = User.objects.all()
-	serializer_class = UserSerializer;
+    """
+    A simple ViewSet for listing or retrieving users.
+    """
+    queryset            = User.objects.all()
+    serializer_class    = UserSerializer
+    http_method_names = ['get',]
+    def list(self, request):
+
+        queryset    = User.objects.filter(is_staff=False)
+        serializer  = UserSerializer(queryset, many=True)
+
+        return Response(serializer.data)
+
+    def create(self, request):
+        data ={}
+        data['Failure']="This method is not allowed"
+        return Response(data, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+
+class BlogViewSet(viewsets.ModelViewSet):
+
+    queryset            = Blog.objects.filter(private=False)
+    serializer_class    = BlogSerializer
+    http_method_names =['get',]
+
+
+# connecting the ViewSets
+
+user_list = UserViewSet.as_view({'get': 'list'})
+
 
 
 # Router for autometic URL config
-router = routers.DefaultRouter()
-router.register(r'users',UserViewSet)
+router    = routers.DefaultRouter()
+
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'blogs',BlogViewSet, basename='blogs')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blogs.urls')),
     path('api/', include(router.urls)),
-    path('user-info/', include('rest_framework.urls', namespace='rest_framework'))
+    #path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
